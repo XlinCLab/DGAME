f8f7a567ef5fe498555137c1c0f4e60dad6d59d3
Philip Georgis <pgeorgis@uni-koeln.de>
2025-09-04 14:09:19 +0200

    Share input validation function between steps A and F

--- dgame/F_preproc_EEG.py
+++ dgame/F_preproc_EEG.py
@@ -3,6 +3,7 @@ import logging
 import os
 
 from dgame.constants import STEP_F_KEY
+from dgame.input_validation import retrieve_and_validate_inputs
 from experiment.load_experiment import Experiment
 
 logger = logging.getLogger(__name__)
@@ -14,12 +15,9 @@ def main(experiment: str | dict | Experiment) -> Experiment:
         from dgame.dgame import DGAME
         experiment = DGAME.from_input(experiment)
 
-    # Get list of subject IDs and their corresponding XDF directory paths
-    subject_xdf_dirs_dict = experiment.get_subject_dirs_dict(experiment.xdf_indir)
-    subject_ids = list(subject_xdf_dirs_dict.keys())
-    subject_xdf_dirs = list(subject_xdf_dirs_dict.values())
-    assert all(len(subj_xdf_dir) == 1 for subj_xdf_dir in subject_xdf_dirs)
-    subject_xdf_dirs = [subj_xdf_dir[0] for subj_xdf_dir in subject_xdf_dirs]
+    # Retrieve and validate input directories and files
+    # Get flattened lists of subject IDs and per-subject xdf directories as input to MATLAB script
+    subject_ids, subject_xdf_dirs = retrieve_and_validate_inputs(experiment)
 
     # Retrieve list of electrodes which were removed to fit eyetracking glasses
     removed_electrodes = experiment.get_dgame_step_parameter(STEP_F_KEY, "removed_electrodes")
