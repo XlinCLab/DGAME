9860668eba61af114038fce0f694e50eeedb1227
Philip Georgis <pgeorgis@uni-koeln.de>
2025-09-15 12:15:47 +0200

    Annotate fixation times

--- dgame/Ia_plot_rerps.py
+++ dgame/Ia_plot_rerps.py
@@ -1,6 +1,9 @@
 import argparse
 import os
 
+import numpy as np
+import pandas as pd
+
 from dgame.constants import (CHANNEL_FIELD, ERP_FIXATION_FILE_SUFFIX,
                              ERP_NOUN_FILE_SUFFIX)
 from dgame.Ja_lm_permute_and_plot_fixations import \
@@ -59,6 +62,28 @@ def main(experiment: str | dict | Experiment) -> Experiment:
 
         # Annotate both dataframes for laterality and saggitality
         erp_noun_data, erp_fixation_data = map(annotate_laterality_and_saggitality, [erp_noun_data, erp_fixation_data])
+
+        # Annotate ERP fixation data with fixation time labels
+        fix_time_conditions = [
+            (erp_fixation_data["trial_time"] < 0),    # before_noun
+            (erp_fixation_data["trial_time"] >= 0) & (erp_fixation_data["trial_time"] <= 0.5),  # during_noun
+            (erp_fixation_data["trial_time"] > 0.471) & (erp_fixation_data["trial_time"] <= 1)  # after_noun
+        ]
+        fix_time_labels = [
+            "before_noun",  # TODO add as constants, also used in Ja script
+            "during_noun",
+            "after_noun",
+        ]
+        erp_fixation_data["fix_time"] = np.select(fix_time_conditions, fix_time_labels, default=pd.NA)
+
+        # Annotate ERP noun data weith mean target fixation time labels
+        noun_fix_time_conditions = [
+            (erp_noun_data["mean_target_fixation"] < 0),    # before_noun
+            (erp_noun_data["mean_target_fixation"] >= 0) & (erp_noun_data["mean_target_fixation"] <= 0.471),  # during_noun
+            (erp_noun_data["mean_target_fixation"] > 0.471)  # after_noun
+        ]
+        noun_fix_time_labels = fix_time_labels
+        erp_noun_data["mean_target_fixation"] = np.select(noun_fix_time_conditions, noun_fix_time_labels, default=pd.NA)
 
     return experiment
 
