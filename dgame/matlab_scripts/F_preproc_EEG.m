eb51686d8f45386be3a5aa1110bcffa8598c4335
Philip Georgis <philip.georgis@gmail.com>
2026-02-26 12:27:21 +0100

    Clean up MATLAB script F

--- dgame/matlab_scripts/F_preproc_EEG.m
+++ dgame/matlab_scripts/F_preproc_EEG.m
@@ -1,73 +1,74 @@
-function F_preproc_EEG(subject_ids, subject_dirs, experiment_root, experiment_outdir, ica_outdir, matlab_root, dgame_version, removed_electrodes, channels_to_remove_per_subj)
+function F_preproc_EEG(subject_ids, subject_dirs, experiment_root, ...
+    experiment_outdir, ica_outdir, matlab_root, ...
+    dgame_version, removed_electrodes, channels_to_remove_per_subj)
 
 blocks = {'11','12','21','22'};
 
-% Mount dependencies / toolboxes
+%% ------------------------------------------------------------------------
+% EEGLAB
+% -------------------------------------------------------------------------
 cd(matlab_root);
 addpath('./eeglab2021.1');
 eeglab;
 
-chanlocs = fullfile(matlab_root, 'eeglab2021.1', 'plugins', 'dipfit', 'standard_BESA', 'standard-10-5-cap385.elp');
+chanlocs_file = fullfile(matlab_root, 'eeglab2021.1', 'plugins', 'dipfit', 'standard_BESA', 'standard-10-5-cap385.elp');
 
+%% ========================================================================
 for s = 1:length(subject_ids)
-    clear EEG
-    T = 0;
+    clear EEG EEG_raw TMP
     subj = subject_ids{s};
-    subject_xdf_dir = subject_dirs{s};
-    subject_xdf_dir = string(subject_xdf_dir);
+    subject_xdf_dir = string(subject_dirs{s});
     raw_set_before_filtering = [subj,'_raw_before_filtering.set'];
     pre_ica_file = [subj,'_director_preICA.set'];
     post_ica_file = [subj,'_director_postIC.set'];
     cleaned_set = [subj,'_director_cleaned.set'];
     all_ics_set = [subj,'_director_allICs.set'];
-    outpath = fullfile(experiment_outdir, 'eeg', subj);
-    subj_ica_outdir = fullfile(ica_outdir, subj);
-    if ~isfolder(outpath)
-        mkdir outpath;
-    end
 
-    %% load data
+    outpath = fullfile(experiment_outdir,'eeg',subj);
+    subj_ica_outdir = fullfile(ica_outdir,subj);
+    if ~isfolder(outpath); mkdir(outpath); end
+    if ~isfolder(subj_ica_outdir); mkdir(subj_ica_outdir); end
+
+    %% =====================================================================
+    % ====================== LOAD & MERGE BLOCKS ===========================
+    %% =====================================================================
+
     for b = 1:length(blocks)
         block = string(blocks{b});
-        trialtime_filename = sprintf('%s_words2erp_%s_trialtime.csv', subj, block);
-        event_file = fullfile(experiment_outdir, 'audio', subj, trialtime_filename);
-        event_file = char(event_file);
-        fixations_filename = sprintf('fixations_times_%s_trials.csv', block);
-        fixations_file = fullfile(experiment_root, 'preproc', 'eyetracking', 'fixations', subj, fixations_filename);
-        fixations_files = char(fixations_file);
+
+        % ----------- LOAD XDF ------------------------------
         xdfFile = fullfile(subject_xdf_dir, 'Director', "dgame" + string(dgame_version) + "_" + subj + "_Director_" + block + ".xdf");
         xdfFile = char(xdfFile);
-
-        %load the data
         [tmp_EEG] = pop_loadxdf(xdfFile, 'streamtype', 'EEG');
 
-        %% read word data
-        tmp_EEG.event = table2struct(readtable(event_file));
+        %% -------------------- LOAD WORD EVENTS ---------------------------
+        trialtime_filename = sprintf('%s_words2erp_%s_trialtime.csv',subj,block);
+        event_file = fullfile(experiment_outdir,'audio',subj,trialtime_filename);
+        event_file = char(event_file);
 
-        %add fields inside fixations but not words to have matching field names
-        [tmp_EEG.event.saccAmpl] = deal([]);
-        [tmp_EEG.event.fix_at] = deal([]);
-        [tmp_EEG.event.latency] = deal([]);
-        [tmp_EEG.event.trial_time_char] = tmp_EEG.event.trial_time;
-        [tmp_EEG.event.trial_time] = deal([]);
+        words = table2struct(readtable(event_file));
+        [words.saccAmpl] = deal([]);
+        [words.fix_at] = deal([]);
+        [words.latency] = deal([]);
+        [words.trial_time_char] = tmp_EEG.event.trial_time;
+        [words.trial_time] = deal([]);
 
-        for ev=1:length(tmp_EEG.event)
-            tmp_EEG.event(ev).latency = tmp_EEG.event(ev).time*tmp_EEG.srate;
-            tmp_EEG.event(ev).duration = tmp_EEG.event(ev).tmax-tmp_EEG.event(ev).time;
-            tmp_EEG.event(ev).type = tmp_EEG.event(ev).pos;
-            if strcmp(tmp_EEG.event(ev).trial_time_char, 'NA') == 0
-                tmp_EEG.event(ev).trial_time = str2num(tmp_EEG.event(ev).trial_time_char);
+        for ev = 1:length(words)
+            words(ev).latency = words(ev).time * tmp_EEG.srate;
+            words(ev).duration = words(ev).tmax - words(ev).time;
+            words(ev).type = words(ev).pos;
+            if strcmp(words(ev).trial_time_char, 'NA') == 0
+                words(ev).trial_time = str2num(words(ev).trial_time_char);
             end
         end
-        
-        %% read fixation data
-        fixations = table2struct(readtable(fixations_file));
-        for fix = 1:length(fixations)
-            fixations(fix).type = 'fixation';
-            fixations(fix).latency = fixations(fix).time * 500;
-        end
-            
-        %create fields so word and fixation events have the same fields
+
+        %% -------------------- LOAD FIXATIONS ------------------------------
+        fix_filename = sprintf('fixations_times_%s_trials.csv',block);
+        fix_file = fullfile(experiment_root,'preproc','eyetracking','fixations',subj,fix_filename);
+        fix_file = char(fix_file);
+
+        fixations = table2struct(readtable(fix_file));
+
         fixation_events = [];
         [fixation_events.id] = deal([]);
         [fixation_events.time] = deal([]);
@@ -103,22 +104,24 @@ for s = 1:length(subject_ids)
         [fixation_events.trial_time_char] = deal([]);
        
         for ev = 1:length(fixations)
-            fixation_events(ev).latency = fixations(ev).latency;
+            fixation_events(ev).type = 'fixation';
+            fixation_events(ev).latency = fixations(fix).time * 500;
             fixation_events(ev).duration = fixations(ev).duration;
-            fixation_events(ev).type = fixations(ev).type;
             fixation_events(ev).fix_at = fixations(ev).fix_at;
             fixation_events(ev).trial_time = fixations(ev).trial_time;
             if fixations(ev).saccAmpl > 0
                 fixation_events(ev).saccAmpl = fixations(ev).saccAmpl;
             end
         end
-        
-        tmp_EEG.event = [tmp_EEG.event;fixation_events'];
+
+        % Merge word and fixation events
+        tmp_EEG.event = [words; fixation_events'];
         tmp_EEG = eeg_checkset(tmp_EEG,'eventconsistency');
-            
-%% resample and merge
-        tmp_EEG = pop_resample(tmp_EEG, 250);
-        tmp_EEG=pop_chanedit(tmp_EEG, 'lookup',chanlocs);
+
+        %% -------------------- RESAMPLE BEFORE MERGE ----------------------
+        tmp_EEG = pop_resample(tmp_EEG,250);
+        tmp_EEG = pop_chanedit(tmp_EEG,'lookup',chanlocs_file);
+
         if exist('EEG','var')
             EEG = pop_mergeset(EEG,tmp_EEG);
             EEG = eeg_checkset(EEG);
@@ -126,114 +129,184 @@ for s = 1:length(subject_ids)
             EEG = tmp_EEG;
         end
     end
-%interims save
+    % Interim save
     pop_saveset(EEG,'filename',raw_set_before_filtering,'filepath',outpath);
 
     EEG_raw = EEG;
-%% pre clean data
-    EEG = pop_eegfiltnew(EEG, 'locutoff',2);
-%exlude the channels that were removed to fit the ET glasses
+
+    %% =====================================================================
+    % ============================ CLEANING ================================
+    %% =====================================================================
+    % Pre-clean data
+    EEG = pop_eegfiltnew(EEG,'locutoff',2);
+    % Exclude channels that were removed to fit ET glasses
     EEG = pop_select(EEG,'nochannel',removed_electrodes);
-    % exclude any other channels, e.g. due to broken electrodes or excessive noise
+    % Exclude any other specified channels, e.g. due to broken electrodes or excessive noise
     channels_to_remove = channels_to_remove_per_subj{s};
-    if length(channels_to_remove) > 0
+    if ~isempty(channels_to_remove)
         fprintf('Subject %s: Removing channels: %s\n', subj, ...
             strjoin(cellfun(@num2str, channels_to_remove, 'UniformOutput', false), ', '));
         EEG = pop_select(EEG,'nochannel',channels_to_remove);
     end
 
-%use clean_raw_data
-    EEG = pop_clean_rawdata(EEG, 'FlatlineCriterion',5,'ChannelCriterion',0.8,'LineNoiseCriterion',4,'Highpass','off','BurstCriterion','off','WindowCriterion','off','BurstRejection','off','Distance','Euclidian');
-    EEG = pop_rejchan(EEG, 'elec',[1:EEG.nbchan] ,'threshold',2,'norm','on','measure','kurt');
-    
+    EEG = pop_clean_rawdata(EEG,...
+        'FlatlineCriterion',5,...
+        'ChannelCriterion',0.8,...
+        'LineNoiseCriterion',4,...
+        'Highpass','off',...
+        'BurstCriterion','off',...
+        'WindowCriterion','off',...
+        'BurstRejection','off',...
+        'Distance','Euclidian');
+    EEG = pop_rejchan(EEG,...
+        'elec',[1:EEG.nbchan],...
+        'threshold',2,...
+        'norm','on',...
+        'measure','kurt');
+
     pools = gcp('nocreate');
     cpus = feature('numCores');
     if size(pools) == 0
         pool = parpool(cpus);
     end
-%filter at 100 Hz low-pass
-    EEG = pop_eegfiltnew(EEG, 'hicutoff',100,'plotfreqz',0);
-%run cleanline to remove line noise
-    EEG = pop_cleanline(EEG, 'bandwidth',2,'chanlist',[1:EEG.nbchan] ,'computepower',1,'linefreqs',[50] ,'newversion',1,'normSpectrum',0,'p',0.01,'pad',2,'plotfigures',0,'scanforlines',0,'sigtype','Channels','taperbandwidth',2,'tau',100,'verb',1,'winsize',4,'winstep',1);
-    EEG=pop_chanedit(EEG, 'lookup',chanlocs);
-    burstCriterion = 10;
-    burstRejection = 'off';
-    EEG = pop_clean_rawdata(EEG, 'FlatlineCriterion','off','ChannelCriterion','off','LineNoiseCriterion','off','Highpass','off','BurstCriterion',burstCriterion,'WindowCriterion','off','BurstRejection',burstRejection,'Distance','Euclidian');
+        
+    % Low-pass filter at 100 Hz
+    EEG = pop_eegfiltnew(EEG,
+        'hicutoff',100,...
+        'plotfreqz',0);
 
-%%find and interpolate missing channels using the EEG_raw copy from above
+    % Run cleanline to remove line noise
+    EEG = pop_cleanline(EEG,...
+        'bandwidth',2,...
+        'linefreqs',50,...
+        'chanlist',[1:EEG.nbchan],...
+        'computepower',1,...
+        'linefreqs',[50],...
+        'newversion',1,...
+        'normSpectrum',0,...
+        'p',0.01,...
+        'pad',2,...
+        'plotfigures',0,...
+        'scanforlines',0,...
+        'sigtype',...
+        'Channels',...
+        'taperbandwidth',2,...
+        'tau',100,...
+        'verb',1,...
+        'winsize',4,...
+        'winstep',1);
+    EEG = pop_chanedit(EEG, 'lookup',chanlocs);
+    EEG = pop_clean_rawdata(EEG,...
+        'FlatlineCriterion','off',...
+        'ChannelCriterion','off',...
+        'LineNoiseCriterion','off',...
+        'Highpass','off',...
+        'BurstCriterion',10,...
+        'WindowCriterion','off',...
+        'BurstRejection','off',...
+        'Distance','Euclidian');
+
+    %% ==================== INTERPOLATE MISSING CHANNELS ===========================
     all_chans = {EEG_raw.chanlocs.labels}';
-    same_chans = intersect({EEG.chanlocs.labels}',{EEG_raw.chanlocs.labels}');
-    bad_chans=setdiff(all_chans,same_chans);
+    kept_chans = {EEG.chanlocs.labels}';
+    bad_chans = setdiff(all_chans,kept_chans);
     EEG.bad_chans = bad_chans;
-    %interpolate   
     EEG = pop_interp(EEG,EEG_raw.chanlocs,'spherical');
-%% re-reference to average, adding and again removing the initial Reference    
+
+    %% ===================== AVERAGE REFERENCE =============================
+    % Re-reference to average, adding and again removing the initial reference
     EEG.nbchan = EEG.nbchan+1;
     EEG.data(end+1,:) = zeros(1, EEG.pnts);
     EEG.chanlocs(1,EEG.nbchan).labels = 'initialReference';
     EEG = pop_reref(EEG, []);
-    EEG = pop_select( EEG,'nochannel',{'initialReference'});
+    EEG = pop_select(EEG,'nochannel',{'initialReference'});
     EEG = pop_select(EEG,'nochannel',bad_chans);
-    EEG = eeg_checkset( EEG );
+    EEG = eeg_checkset(EEG);
 
-%save interims file
+    % Save interim pre-ICA file
     EEG = pop_saveset(EEG,'filename',pre_ica_file,'filepath',outpath);
 
-%% run amica
-    % Run ICA
-    % define parameters
-    dataRank = sum(eig(cov(double(EEG.data'))) > 1E-6); 
-    numprocs= 1;       % # of nodes
-    max_threads= 10;   % # of threads
-    num_models= 1;     % # of models of mixture ICA
-    max_iter= 20;    % max number of learning steps % run amica #20 for testing, set to 2000 at least
-    num_rej = 10;      % # of rejections of unlikely data
-
+    %% =====================================================================
+    % =============================== ICA ==================================
+    %% =====================================================================
+    % Define parameters for amica
+    dataRank = sum(eig(cov(double(EEG.data'))) > 1E-6);
+    numprocs= 1;        % number of nodes
+    max_threads= 10;    % number of threads
+    num_models= 1;      % number of models of mixture ICA
+    max_iter= 20;       % max number of learning steps % run amica #20 for testing, set to 2000 at least
+    num_rej = 10;       % # of rejections of unlikely data
     EEG = pop_resample(EEG,100);
-% run the actual decomposition
-    runamica15(EEG.data,'num_chans', EEG.nbchan,'outdir',subj_ica_outdir,'pcakeep',dataRank,'num_models', num_models,'do_reject', 1,'numrej', num_rej,'rejsig', 2.5,'rejint', 1,'max_iter',max_iter,'numprocs',numprocs,'max_threads',max_threads);
-    EEG.etc.amica  = loadmodout15(subj_ica_outdir);
-    EEG.icaweights = EEG.etc.amica.W;
-    EEG.icasphere  = EEG.etc.amica.S;
+    % Run the actual decomposition
+    runamica15(EEG.data,...
+        'num_chans',EEG.nbchan,...
+        'outdir',subj_ica_outdir,...
+        'pcakeep',dataRank,...
+        'num_models', num_models,...
+        'do_reject',1,...
+        'numrej', num_rej,...
+        'rejsig', 2.5,...
+        'rejint', 1,...
+        'max_iter',max_iter,...
+        'numprocs',numprocs,...
+        'max_threads',max_threads);
+
+    mod = loadmodout15(subj_ica_outdir);
+    EEG.icaweights = mod.W;
+    EEG.icasphere  = mod.S;
     EEG = eeg_checkset(EEG, 'ica');
     EEG = pop_saveset(EEG, 'filename',post_ica_file,'filepath',outpath);
-%% clean data using ICs
-    all_chans = {EEG_raw.chanlocs.labels}';
-    same_chans = intersect({EEG.chanlocs.labels}',{EEG_raw.chanlocs.labels}');
-    bad_chans=setdiff(all_chans,same_chans);
-    %write ica info to temporary structure to transfer to uncleaned data
+
+    % Clean data using ICs
+    % Write ICA info to temporary structure to transfer to uncleaned data
     TMP.icasphere = EEG.icasphere;
     TMP.icaweights = EEG.icaweights;
-    EEG_raw.bad_chans = bad_chans;   
-%use "raw" data again for different filtering etc.
+    EEG_raw.bad_chans = bad_chans;
+
+    % Use "raw" data again for different filtering etc.
     EEG = EEG_raw;
     EEG = pop_select(EEG,'nochannel',EEG.bad_chans);
     EEG = pop_eegfiltnew(EEG, 0.3,[]);% 'ftype', 'highpass', 'wtype', 'hamming', 'forder', 826, 'minphase', 0, 'usefftfilt', 0, 'plotfresp', 0, 'causal', 0);
     EEG = pop_eegfiltnew(EEG,[],20);
-    EEG=pop_chanedit(EEG, 'lookup',chanlocs);
-% asr again
-    burstCriterion = 10;
-    burstRejection = 'off';
-    EEG = pop_clean_rawdata(EEG, 'FlatlineCriterion','off','ChannelCriterion','off','LineNoiseCriterion','off','Highpass','off','BurstCriterion',burstCriterion,'WindowCriterion','off','BurstRejection',burstRejection,'Distance','Euclidian');
-    EEG=pop_chanedit(EEG, 'lookup',chanlocs);
-%interpolate channels
+    EEG = pop_chanedit(EEG, 'lookup',chanlocs);
+
+    % ASR again
+    EEG = pop_clean_rawdata(EEG,...
+        'FlatlineCriterion','off',...
+        'ChannelCriterion','off',...
+        'LineNoiseCriterion','off',...
+        'Highpass','off',...
+        'BurstCriterion',10,...
+        'WindowCriterion','off',...
+        'BurstRejection','off',...
+        'Distance','Euclidian');
+    EEG = pop_chanedit(EEG, 'lookup',chanlocs);
+
+    % Interpolate channels
     EEG = pop_interp(EEG,EEG_raw.chanlocs);
     EEG = eeg_checkset(EEG);
     EEG.nbchan = EEG.nbchan+1;
     EEG.data(end+1,:) = zeros(1, EEG.pnts);
     EEG.chanlocs(EEG.nbchan).labels = 'initialReference';
-%average reference
+
+    % Average reference
     EEG = pop_reref(EEG, []);
     EEG = pop_select( EEG,'nochannel',{'initialReference'});
     EEG = pop_select(EEG,'nochannel',bad_chans);
     EEG.badchans = bad_chans;
-%transfer ICA weights and sphere from TMP        
+
+    % Transfer ICA weights and sphere from TMP structure    
     EEG.icaweights = TMP.icaweights;
     EEG.icasphere = TMP.icasphere;
     EEG = eeg_checkset(EEG,'ica');
-% classify components using iclabel
+    
+    %% =====================================================================
+    % =========================== IC LABEL =================================
+    %% =====================================================================
+    % Classify components using IC label
     EEG = pop_iclabel(EEG, 'default');
-%mark brain and non brain components in a vector of booleans
+
+    % Mark brain and non-brain components in a vector of booleans
     classifications = EEG.etc.ic_classification.ICLabel.classifications;
     [classes ~] = find(transpose(EEG.etc.ic_classification.ICLabel.classifications == max(EEG.etc.ic_classification.ICLabel.classifications,[],2)));
     ICs_keep = zeros(length(classes),1);
@@ -245,12 +318,19 @@ for s = 1:length(subject_ids)
     end
     EEG.ICs_throw = find(~ICs_keep);
     EEG.ICs_keep = find(ICs_keep);
-%interims save
+
+    % Interim save
     EEG = pop_saveset(EEG,'filename',all_ics_set,'filepath',outpath);
-%remove non-brain components
+
+    % Remove non-brain components
     EEG = pop_subcomp(EEG,[EEG.ICs_throw]);
-%interpolate missing channels again
+
+    %% ================= FINAL INTERPOLATION ===============================
+    % Interpolate missing channels again
     EEG = pop_interp(EEG,EEG_raw.chanlocs);
-%save set
-    EEG = pop_saveset(EEG,'filename',cleaned_set,'filepath',outpath);
-end  
+
+    % Save set
+    pop_saveset(EEG,'filename',cleaned_set,'filepath',outpath);
+
+end
+end
