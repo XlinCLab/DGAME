283dad18cf263f625610e3cc677d53f1127f495b
Philip Georgis <philip.georgis@gmail.com>
2026-06-18 14:56:19 +0200

    Sort imports

--- dgame/dgame.py
+++ dgame/dgame.py
@@ -13,9 +13,9 @@ from dgame.constants import (BLOCK_IDS, CHANNEL_COORDS_FILE, CHANNEL_FIELD,
                              DGAME_DEFAULT_CONFIG, GAZE_POSITIONS_FILE,
                              OBJECT_FIELD, OBJECT_POSITIONS_FILE, SCRIPT_DIR,
                              STEP_A_KEY, STEP_B_KEY, STEP_CA_KEY, STEP_CB_KEY,
-                             STEP_CC_KEY, STEP_DA_KEY, STEP_DB_KEY,
-                             STEP_E_KEY, STEP_F_KEY, STEP_G_KEY, STEP_H_KEY,
-                             STEP_I_KEY, STEP_J_KEY, SURFACE_LIST, WORD_FIELD)
+                             STEP_CC_KEY, STEP_DA_KEY, STEP_DB_KEY, STEP_E_KEY,
+                             STEP_F_KEY, STEP_G_KEY, STEP_H_KEY, STEP_I_KEY,
+                             STEP_J_KEY, SURFACE_LIST, WORD_FIELD)
 from dgame.Da_gaze_stats import main as step_da
 from dgame.Db_plot_descriptive_fixation import main as step_db
 from dgame.E_describe_syntactic_patterns_from_audio_instructions import main as step_e
