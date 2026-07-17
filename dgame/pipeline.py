1a19cf6f115f0b4627d2413780def8d8f4c8020e
Philip Georgis <philip.georgis@gmail.com>
2026-07-17 17:45:13 +0200

    Move former step E (words.describe_syntactic_patterns) to come after former step B (words.prepapre_words)

--- dgame/pipeline.py
+++ dgame/pipeline.py
@@ -1,12 +1,12 @@
 # DGAME ANALYSIS STEPS
 STEP_A_KEY = "A_export_audio_and_et_times"
 STEP_B_KEY = "words.prepare_words"
+STEP_E_KEY = "words.describe_syntactic_patterns"
 STEP_CA_KEY = "eyetracking.align_gaze_with_audio"
 STEP_CB_KEY = "eyetracking.compute_fixation_saccades"
 STEP_CC_KEY = "eyetracking.label_fixation_trials"
 STEP_DA_KEY = "eyetracking.gaze_language_timecourse_stats"
 STEP_DB_KEY = "eyetracking.plot_descriptive_fixation_stats"
-STEP_E_KEY = "words.describe_syntactic_patterns"
 STEP_F_KEY = "eeg.preprocess_eeg"
 STEP_G_KEY = "eeg.deconvolve_erps"
 STEP_H_KEY = "eeg.reconstruct_erps"
@@ -17,12 +17,12 @@ STEP_J_KEY = "eeg.regression_permutation_stats"
 FULL_DGAME_PIPELINE = [
     STEP_A_KEY,
     STEP_B_KEY,
+    STEP_E_KEY,
     STEP_CA_KEY,
     STEP_CB_KEY,
     STEP_CC_KEY,
     STEP_DA_KEY,
     STEP_DB_KEY,
-    STEP_E_KEY,
     STEP_F_KEY,
     STEP_G_KEY,
     STEP_H_KEY,
